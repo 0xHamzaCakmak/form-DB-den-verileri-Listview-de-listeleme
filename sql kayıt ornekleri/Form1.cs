2f301e817758446d837b2d2ff2a7b2cb041331a8
hamza çakmak <hamzaacakmak>
2019-09-21 23:09:07 +0300

    first commit

---
+++ sql kayıt ornekleri/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace sql_kayıt_ornekleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=Ogrenciler;Integrated Security=True");
+        private void verilerimigöster()
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Select * from Bilgiler", baglanti);
+            SqlDataReader oku = komut.ExecuteReader();
+            while (oku.Read())
+            {
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = oku["Adsoyad"].ToString();
+                ekle.SubItems.Add(oku["Sehir"].ToString());
+                ekle.SubItems.Add(oku["Okul"].ToString());
+                listView1.Items.Add(ekle);
+            }
+            baglanti.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            verilerimigöster();
+        }
+    }
+}
